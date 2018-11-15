350e8f5253445851e948f3ac6599ae06e09f6ffe
diktaafif <afifdikta@gmail.com>
2018-11-15 13:23:33 +0700

    tema

--- app/src/main/java/com/example/ulfa/itmaps/MainActivity_login.java
+++ app/src/main/java/com/example/ulfa/itmaps/MainActivity_login.java
@@ -1,13 +1,61 @@
 package com.example.ulfa.itmaps;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity_login extends AppCompatActivity {
-
+    Dialog popup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_login);
+
+        popup = new Dialog(this);
+
+        TextView textView = findViewById(R.id.textView);
+
+        String text = "Jika ada masalah silakan Klik Disini";
+
+        Spannable ss = new SpannableString(text);
+
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View view) {
+                TextView txtclose;
+                popup.setContentView(R.layout.popwindow);
+                txtclose = (TextView) popup.findViewById(R.id.textViewOKE);
+                txtclose.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        popup.dismiss();
+                    }
+                });
+                popup.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                popup.show();
+            }
+
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setColor(Color.BLUE);
+                ds.setUnderlineText(true);
+            }
+        };
+        ss.setSpan(clickableSpan,30,36,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        textView.setText(ss);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
--- app/src/main/java/com/example/ulfa/itmaps/SplashScreen.java
+++ app/src/main/java/com/example/ulfa/itmaps/SplashScreen.java
@@ -1,13 +1,26 @@
 package com.example.ulfa.itmaps;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatImageView;
 
 public class SplashScreen extends AppCompatActivity {
 
+    private static int SPLASH_TIME_OUT = 5000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent login = new Intent(SplashScreen.this, MainActivity_login.class);
+                startActivity(login);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 }
