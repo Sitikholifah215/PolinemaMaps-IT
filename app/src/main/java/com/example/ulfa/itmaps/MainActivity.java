95f21d6a6218727d708d809163345789e1cb6db0
Sitikholifah215 <skholifah215@gmail.com>
2018-11-27 09:11:35 +0700

    fix splashscreen

--- app/src/main/java/com/example/ulfa/itmaps/MainActivity.java
+++ app/src/main/java/com/example/ulfa/itmaps/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.ulfa.itmaps;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int SPLASH_TIME_OUT = 5000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent login = new Intent(MainActivity.this, MainActivity_login.class);
+                startActivity(login);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 }
