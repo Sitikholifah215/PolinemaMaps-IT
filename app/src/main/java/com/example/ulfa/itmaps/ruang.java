19c8daedc95a48f20a95ebd1600fbbb75f4271db
sulistyohabib <44793089+sulistyohabib@users.noreply.github.com>
2018-11-30 09:00:08 +0700

    ruangan

---
+++ app/src/main/java/com/example/ulfa/itmaps/ruang.java
@@ -0,0 +1,12 @@
+package com.example.ulfa.itmaps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ruang{
+
+}
