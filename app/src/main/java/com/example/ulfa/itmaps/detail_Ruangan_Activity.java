f5bd250afc41159c7432eb7e8125f55d9043a8fb
diktaafif <afifdikta@gmail.com>
2018-12-09 19:33:23 +0700

    Layout
    Penambahan detail

---
+++ app/src/main/java/com/example/ulfa/itmaps/detail_Ruangan_Activity.java
@@ -0,0 +1,13 @@
+package com.example.ulfa.itmaps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class detail_Ruangan_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail__ruangan_);
+    }
+}
