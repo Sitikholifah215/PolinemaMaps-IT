a59f4dc600e11cdee2737a18197e0110902e4a8b
diktaafif <afifdikta@gmail.com>
2018-12-24 15:02:27 +0700

    search

--- app/src/main/java/com/example/ulfa/itmaps/MainActivity_ruangan.java
+++ app/src/main/java/com/example/ulfa/itmaps/MainActivity_ruangan.java
@@ -6,8 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.ulfa.itmaps.Adapter.Rec_slide_adapter;
@@ -19,11 +24,16 @@ import com.example.ulfa.itmaps.Rest.ApiInterface;
 import com.example.ulfa.itmaps.listener.ClickListener;
 import com.example.ulfa.itmaps.listener.RecyclerTouchListener;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Queue;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import retrofit2.http.Query;
 
 public class MainActivity_ruangan extends AppCompatActivity {
     RecyclerView mRuanganview;
@@ -31,13 +41,31 @@ public class MainActivity_ruangan extends AppCompatActivity {
     RecyclerView.Adapter mAdapter;
     Context mContext;
     ApiInterface mApiInterface;
+    ListView pencarian_ruangan;
+    ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_ruangan);
         initRecyclerRuangan();
+
+        pencarian_ruangan = (ListView) findViewById(R.id.rv_main);
+
+        ArrayList<String> arrayRuangan = new ArrayList<>();
+        //iki seng error gak paham nyeluk e
+        arrayRuangan.addAll(Arrays.asList(getResources().getStringArray(R.array.cari_Ruangan)));
+
+        adapter = new ArrayAdapter<String>(
+                MainActivity_ruangan.this,
+                android.R.layout.simple_list_item_1,
+                arrayRuangan
+        );
+        pencarian_ruangan.setAdapter(adapter);
     }
+
+    public boolean onCreateOptions
 
     private void initRecyclerRuangan() {
         mContext = getApplicationContext();
